1828353d43fb4a207ee69b1cf94c85fe745fa86b
santi <santiago.cabrera0806@gmail.com>
2021-04-27 01:23:03 +0200

    Se hace navbar responsive

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,54 @@
 import React, {Component} from 'react';
+import './navbarevents.js';
+import {FaBars} from 'react-icons/fa';
 
-class MyHeader extends Component{
+const navbarItems = [ 
+  {"text":"Inicio", "url":"#Inicio"},
+  {"text":"Skills", "url":"#Skills"},
+  {"text":"Experiencia", "url":"#Experiencia"},
+  {"text":"Contacto", "url":"#Contacto"},
+  {"text":"Portfolio", "url":"#Portfolio"}
+];
+
+function handleClick(e) {    
+  e.preventDefault();    
+  console.log('The link was clicked.');
+  let items = document.getElementsByClassName("navbarItem"); 
+  
+  console.log(items);
+
+  for (let index = 0; index < items.length; index++) {
+    const element = items[index];
     
-  render(){
-    return (
-      <header className="App-header">
-        <div className="logo-banner"></div>
-        <nav className="navbar">
-          <a href="#Inicio">Inicio</a>
-          <a href="#Skills">Skills</a>
-          <a href="#Experiencia">Experiencia</a>
-          <a href="#Contacto">Contacto</a> 
-        </nav>
-      </header>
-    );
+    if(element.style.display=="none")
+      element.style.display = "block";
+    else
+      element.style.display = "none";
   }
 
+}
+
+class MyHeader extends Component{
+
+  render(){
+    return (
+        <header className="App-header"> 
+          <nav className="navbar" role="navigation">
+            
+  
+                <a id="toggler" onClick={handleClick}><FaBars /></a>
+              
+                {navbarItems.map((item, i) => {     
+                  console.log(item.text);
+                  return(<a key={i} href={item.url} className="navbarItem">{item.text}</a>)                 
+                })}
+              
+            
+          </nav>
+        </header>
+    );
+
+  }
 }
 
 export default MyHeader;
